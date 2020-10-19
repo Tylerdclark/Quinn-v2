bf5a1ed900402c7933adeec2ebdd60c3e0281122
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-18 23:32:43 -0400

    set useragent and start hast command (will need to rename)

--- src/main/java/me/tylerdclark/quinn/Bot.java
+++ src/main/java/me/tylerdclark/quinn/Bot.java
@@ -1,5 +1,6 @@
 package me.tylerdclark.quinn;
 
+import me.duncte123.botcommons.web.WebUtils;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -9,8 +10,11 @@ import javax.security.auth.login.LoginException;
 public class Bot {
 
 
-
     public static void main(String[] arguments) throws LoginException {
+
+
+        WebUtils.setUserAgent("Mozilla/5.0 Quinn Bot v2#2605 / SoMuchClark#1766");
+
         JDA api = JDABuilder.createDefault(Config.get("TOKEN"))
                 .setActivity(Activity.watching("Humans age."))
                 .addEventListeners(new Listener())
