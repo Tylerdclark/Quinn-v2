646542eaf25714daec73187dfa99286cf220586a
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-22 14:49:05 -0400

    Webhook command

--- src/main/java/me/tylerdclark/quinn/command/commands/WebhookCommand.java
+++ src/main/java/me/tylerdclark/quinn/command/commands/WebhookCommand.java
@@ -1,2 +1,61 @@
-package me.tylerdclark.quinn.command.commands;public class WebhookCommand {
+package me.tylerdclark.quinn.command.commands;
+
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.WebhookClientBuilder;
+import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+import me.tylerdclark.quinn.Config;
+import me.tylerdclark.quinn.command.CommandContext;
+import me.tylerdclark.quinn.command.ICommand;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.List;
+
+public class WebhookCommand implements ICommand {
+
+    private final WebhookClient client;
+
+    public WebhookCommand() {
+
+        WebhookClientBuilder builder = new WebhookClientBuilder(Config.get("WEBHOOK_URL")); // or id, token
+        builder.setThreadFactory((job) -> {
+            Thread thread = new Thread(job);
+            thread.setName("Hello");
+            thread.setDaemon(true);
+            return thread;
+        });
+
+       this.client = builder.build();
+    }
+
+    @Override
+    public void handle(CommandContext ctx) {
+        final List<String> args = ctx.getArgs();
+        final TextChannel textChannel = ctx.getChannel();
+
+        if (args.isEmpty()){
+            textChannel.sendMessage("Missing arguments").queue();
+            return;
+        }
+
+        final User user = ctx.getAuthor();
+
+        WebhookMessageBuilder builder = new WebhookMessageBuilder()
+                .setUsername(user.getName()) //ctx.getMember().getEffectiveName()
+                .setAvatarUrl(user.getEffectiveAvatarUrl().replaceFirst("gif", "png") + "?size=512")
+                .setContent(String.join(" ", args));
+
+        client.send(builder.build());
+    }
+
+    @Override
+    public String getName() {
+        return "webhook";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Send a webhook message as your name\n" +
+                "Usage: `!!webhook [message]`";
+    }
 }
