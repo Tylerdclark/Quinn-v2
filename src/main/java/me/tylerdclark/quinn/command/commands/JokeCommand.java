2562fd3c11596d0b931ae8832ed319d3a4ad2af7
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-22 14:49:16 -0400

    joke command

--- src/main/java/me/tylerdclark/quinn/command/commands/JokeCommand.java
+++ src/main/java/me/tylerdclark/quinn/command/commands/JokeCommand.java
@@ -1,2 +1,43 @@
-package me.tylerdclark.quinn.command.commands;public class JokeCommand {
+package me.tylerdclark.quinn.command.commands;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import me.duncte123.botcommons.web.WebUtils;
+import me.tylerdclark.quinn.command.CommandContext;
+import me.tylerdclark.quinn.command.ICommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+public class JokeCommand implements ICommand {
+    @Override
+    public void handle(CommandContext ctx) {
+        final TextChannel textChannel = ctx.getChannel();
+        WebUtils.ins.getJSONObject("https://apis.duncte123.me/joke").async((json) -> {
+            if (!json.get("success").asBoolean()) {
+                textChannel.sendMessage("Something went wrong, try again later").queue();
+                System.out.println(json);
+                return;
+            }
+
+            final JsonNode data = json.get("data");
+            final String title = data.get("title").asText();
+            final String url = data.get("url").asText();
+            final String body = data.get("body").asText();
+            final EmbedBuilder embed = EmbedUtils.defaultEmbed()
+                    .setTitle(title, url)
+                    .setDescription(body);
+
+            textChannel.sendMessage(embed.build()).queue();
+        });
+    }
+
+    @Override
+    public String getName() {
+        return "joke";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Shows a random joke";
+    }
 }
